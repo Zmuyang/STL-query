62f7dd913f55c52317141f8151d4d39a548eccd8
三三 <18842556043@163.com>
2018-11-15 16:23:04 +0800

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(){
 	//vector 可以用来定义长度可变的数组，也可以用来以邻接表的方式存储图 
 	vector<int> n; // 定义了一个 int 类型的一维数组
+	vector<int> a(10,1); //定义了10个整型元素的变量，且初值为1 
 	vector<vector<int> > N; //定义了二维数组  > > 之间需要保留一个空格 
 	for(int i=0;i<5;i++) n.push_back(i);  // n.pop_back() 删除尾元素  
 	vector<int>::iterator it=n.begin(); //迭代器 
